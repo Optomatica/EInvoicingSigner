dbd694caec2d3116f3bac9347cebcf717ea3def9
Basem Ezzat <basem.taha@optomatica.com>
2022-01-26 19:20:36 +0200

    better formatted and refactored

--- EInvoicingSigner/Signer.cs
+++ EInvoicingSigner/Signer.cs
@@ -10,32 +10,34 @@ using Newtonsoft.Json;
 
 public class TokenSigner
     {
-    private X509Certificate2 selectedCertificate;
+        private X509Certificate2 selectedCertificate;
+        private JObject unsignedInvoice;
+        private String canonicalString;
+        private String cades;
+        private String fullSignedInvoice;
 
-    public static void Main(String[] args)
-    {
-        TokenSigner tokenSigner = new TokenSigner();       
-
-        if (args == null || args.Length < 1 || File.Exists(args[0]) == false)
+        public static void Main(String[] args)
         {
-            Console.WriteLine("document doesnot exist");
-            return;
-        }
+            TokenSigner tokenSigner = new TokenSigner();
 
-        JObject unsignedInvoice = tokenSigner.ReadJsonFromFile(args[0]);
+            if (args == null || args.Length < 1 || File.Exists(args[0]) == false)
+            {
+                Console.WriteLine("document doesnot exist");
+                return;
+                //throw new ArgumentException("Errorneous Argument");
+            }
 
+            tokenSigner.ReadJsonFromFile(args[0]);
 
-        tokenSigner.FindCertificateFromSelector();
-        //Start serialize
-        String canonicalString = tokenSigner.Serialize(unsignedInvoice);
-
-        String cades = tokenSigner.SignWithCMS(canonicalString);
-
-        String fullSignedInvoice = tokenSigner.AddSignatureToInvoice(unsignedInvoice, cades);
-
+            tokenSigner.FindCertificateFromSelector();
         
-        File.WriteAllBytes(Directory.GetCurrentDirectory() + @"\FullSignedDocument.json", System.Text.Encoding.UTF8.GetBytes(fullSignedInvoice));
-    }
+            tokenSigner.Serialize();
+
+            tokenSigner.SignWithCMS();
+
+            tokenSigner.AddSignatureToInvoice();
+            tokenSigner.WriteSignedInvoiceToFile();
+        }
 
         private byte[] HashBytes(byte[] input)
         {
@@ -53,8 +55,9 @@ public class TokenSigner
 
             store.Open(OpenFlags.ReadOnly & OpenFlags.OpenExistingOnly);
             X509Certificate2[]? validCertificates = store.Certificates.Where(x => knownIssures.Any(s => Regex.IsMatch(x.Issuer.ToString(), s))).
-                Where(x => x.NotAfter > DateTime.Today && DateTime.Today > x.NotBefore).
                 Where(x => x.HasPrivateKey).ToArray();
+                //Where(x => x.NotAfter > DateTime.Today && DateTime.Today > x.NotBefore).
+                
 
             X509CertificateCollection certificates = X509Certificate2UI.SelectFromCollection(new X509Certificate2Collection(validCertificates),"Certificates","please select a certificate",X509SelectionFlag.SingleSelection);
 
@@ -62,118 +65,132 @@ public class TokenSigner
             store.Close();
         }
 
-        private JObject ReadJsonFromFile(String FileName)
+        private void ReadJsonFromFile(String FileName)
         {
             // exception 1
 
-        String sourceDocumentJson = File.ReadAllText(FileName);
-
-            JObject documentJson = JsonConvert.DeserializeObject<JObject>(sourceDocumentJson, new JsonSerializerSettings()
+        /*catch (Exception ex)
+        {
+            if (ex is FormatException || ex is OverflowException)
             {
-                FloatFormatHandling = FloatFormatHandling.String,
-                FloatParseHandling = FloatParseHandling.Decimal,
-                DateFormatHandling = DateFormatHandling.IsoDateFormat,
-                DateParseHandling = DateParseHandling.None
-            });
+                WebId = Guid.Empty;
+                return;
+            }
 
-        return documentJson;
-            
+            throw;
+
+        }*/
+            String sourceDocumentJson = File.ReadAllText(FileName);
+
+            this.unsignedInvoice = JsonConvert.DeserializeObject<JObject>(sourceDocumentJson, new JsonSerializerSettings()
+                {
+                    FloatFormatHandling = FloatFormatHandling.String,
+                    FloatParseHandling = FloatParseHandling.Decimal,
+                    DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                    DateParseHandling = DateParseHandling.None
+                });
         }
 
-        private String AddSignatureToInvoice(JObject unsignedInvoice, String cades)
+        private void WriteSignedInvoiceToFile() 
+        {
+            String signedInvoiceName = "SignedInvoice.json";
+            File.WriteAllBytes(Directory.GetCurrentDirectory() +'\\'+ signedInvoiceName, System.Text.Encoding.UTF8.GetBytes(this.fullSignedInvoice));
+        }
+
+        private void AddSignatureToInvoice()
         {
             JObject signaturesObject = new JObject(
                                 new JProperty("signatureType", "I"),
-                                new JProperty("value", cades));
+                                new JProperty("value", this.cades));
 
             JArray signaturesArray = new JArray();
             signaturesArray.Add(signaturesObject);
             unsignedInvoice.Add("signatures", signaturesArray);
-            String fullSignedInvoice = "{\"documents\":[" + unsignedInvoice.ToString() + "]}";
-        return fullSignedInvoice;
+            this.fullSignedInvoice = "{\"documents\":[" + this.unsignedInvoice.ToString() + "]}";            
         }
 
-        public string SignWithCMS(String serializedJson)
+        public void SignWithCMS()
         {
-            byte[] data = Encoding.UTF8.GetBytes(serializedJson);
+            byte[] data = Encoding.UTF8.GetBytes(this.canonicalString);
 
             ContentInfo content = new ContentInfo(new Oid("1.2.840.113549.1.7.5"), data);
             SignedCms cms = new SignedCms(content, true);
 
             EssCertIDv2 bouncyCertificate = new EssCertIDv2(new Org.BouncyCastle.Asn1.X509.AlgorithmIdentifier(new DerObjectIdentifier("1.2.840.113549.1.9.16.2.47")), this.HashBytes(this.selectedCertificate.RawData));
             SigningCertificateV2 signerCertificateV2 = new SigningCertificateV2(new EssCertIDv2[] { bouncyCertificate });
-            CmsSigner signer = new CmsSigner(this.selectedCertificate);
+            
+            CmsSigner signer = new CmsSigner(this.selectedCertificate)
+            {
+                DigestAlgorithm = new Oid("2.16.840.1.101.3.4.2.1")
+            };
 
-            signer.DigestAlgorithm = new Oid("2.16.840.1.101.3.4.2.1");
             signer.SignedAttributes.Add(new Pkcs9SigningTime(DateTime.UtcNow));
             signer.SignedAttributes.Add(new AsnEncodedData(new Oid("1.2.840.113549.1.9.16.2.47"), signerCertificateV2.GetEncoded()));
 
             // exception needs to be handled
             cms.ComputeSignature(signer, false);
-            return Convert.ToBase64String(cms.Encode());
-    }
-
-    //we can give away the serializing here in C# if our serialization is good.
-    public string Serialize(JObject request)
-    {
-        return SerializeToken(request);
-    }
-
-    private string SerializeToken(JToken request)
-    {
-        string serialized = "";
-        if (request.Parent is null)
-        {
-            SerializeToken(request.First);
+            this.cades = Convert.ToBase64String(cms.Encode());
         }
-        else
+
+        public void Serialize()
         {
+            this.canonicalString = SerializeToken(this.unsignedInvoice);
+        }
 
-
-
-            if (request.Type == JTokenType.Property)
+        private string SerializeToken(JToken request)
+        {
+            string serialized = "";
+            if (request.Parent is null)
             {
-                string name = ((JProperty)request).Name.ToUpper();
-                serialized += "\"" + name + "\"";
-                foreach (var property in request)
+                SerializeToken(request.First);
+            }
+            else
+            {
+
+
+
+                if (request.Type == JTokenType.Property)
                 {
-                    if (property.Type == JTokenType.Object)
+                    string name = ((JProperty)request).Name.ToUpper();
+                    serialized += "\"" + name + "\"";
+                    foreach (var property in request)
                     {
-                        serialized += SerializeToken(property);
-                    }
-                    if (property.Type == JTokenType.Boolean || property.Type == JTokenType.Integer || property.Type == JTokenType.Float || property.Type == JTokenType.Date)
-                    {
-                        serialized += "\"" + property.Value<string>() + "\"";
-                    }
-                    if(property.Type == JTokenType.String)
-                    {
-                        serialized +=  JsonConvert.ToString(property.Value<string>()) ;
-                    }
-                    if (property.Type == JTokenType.Array)
-                    {
-                        foreach (var item in property.Children())
+                        if (property.Type == JTokenType.Object)
                         {
-                            serialized += "\"" + ((JProperty)request).Name.ToUpper() + "\"";
-                            serialized += SerializeToken(item);
+                            serialized += SerializeToken(property);
+                        }
+                        if (property.Type == JTokenType.Boolean || property.Type == JTokenType.Integer || property.Type == JTokenType.Float || property.Type == JTokenType.Date)
+                        {
+                            serialized += "\"" + property.Value<string>() + "\"";
+                        }
+                        if(property.Type == JTokenType.String)
+                        {
+                            serialized +=  JsonConvert.ToString(property.Value<string>()) ;
+                        }
+                        if (property.Type == JTokenType.Array)
+                        {
+                            foreach (var item in property.Children())
+                            {
+                                serialized += "\"" + ((JProperty)request).Name.ToUpper() + "\"";
+                                serialized += SerializeToken(item);
+                            }
                         }
                     }
                 }
             }
-        }
-        if (request.Type == JTokenType.Object)
-        {
-            foreach (var property in request.Children())
+            if (request.Type == JTokenType.Object)
             {
-
-                if (property.Type == JTokenType.Object || property.Type == JTokenType.Property)
+                foreach (var property in request.Children())
                 {
-                    serialized += SerializeToken(property);
+
+                    if (property.Type == JTokenType.Object || property.Type == JTokenType.Property)
+                    {
+                        serialized += SerializeToken(property);
+                    }
                 }
             }
+
+            return serialized;
         }
-
-        return serialized;
-    }
-
 }
 
